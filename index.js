898102a1a10ca3a7ee122ed6b9495a764c218269
V1TSO <v.renasco.larrain@gmail.com>
2022-12-03 17:13:14 -0300

    added dotenv and cors

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const { Configuration, OpenAIApi } = require("openai");
+import express from 'express';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+import * as dotenv from 'dotenv';
+dotenv.config()
+
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
 
+const openai = new OpenAIApi(configuration);
 const app = express();
+app.use(cors())
 
 app.get('/api/quiz', async (req, res) => {
     const { input } = req.query;
     if (!input) return res.status(400).send({ error: 'Missing input' });
     try {
+        console.log("A request is being processed");
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `Write the perfect home for a person that likes:\n\n${input}\nDetailed Sentence:`,
@@ -22,11 +28,12 @@ app.get('/api/quiz', async (req, res) => {
             presence_penalty: 0,
         });
         res.send(response.data.choices[0].text);
+        console.log(`Generated!`);
     }
-    catch (err) {
+    catch (error) {
         res.status(400).send(error);
+        console.log("An error occurred");
     }
-
 });
 
 const port = process.env.PORT || 4000;
